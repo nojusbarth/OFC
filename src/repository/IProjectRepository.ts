5ab337e35fe98795774c95c9e9ca766000f0f517
Moritz Nickel <ushnn@student.kit.edu>
2026-01-21 14:09:41 +0100

    remove temporary repo

--- src/repository/IProjectRepository.ts
+++
@@ -1,8 +0,0 @@
-import type { IProject } from "../controller/interface/IProject";
-
-export interface IProjectRepository {
-    getAllProjects(): IProject[];
-    getProjectByName(name: string): IProject | null;
-    addProject(project: IProject): boolean;
-    removeProject(name: string): boolean;
-}
