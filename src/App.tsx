4f1b4783335740aa2401a2d8de81f69c7baefb44
Pascal Kunius <uetaj@student.kit.edu>
2026-01-23 12:22:39 +0100

    Added Bootstrap Functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Bootstrap test
+    <div className="container mt-5">
+      <div className="alert alert-success" role="alert">
+        ✅ Bootstrap funktioniert!
+      </div>
+
+      <button className="btn btn-primary me-2">Primary</button>
+      <button className="btn btn-danger">Danger</button>
     </div>
   );
 }
