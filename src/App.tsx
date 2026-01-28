0342a6e193b87c24a88df1cea968313ed7bf2753
Moritz Nickel <ushnn@student.kit.edu>
2026-01-28 11:39:08 +0100

    demo zeug

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import { Controller } from './controller/logic/Controller';
 import { ProjectRepository } from './repository/ProjectRepository';
 import { Settings } from './controller/logic/Settings';
 import { IController } from './controller/interface/IController';
+import { Vector3 } from 'three';
 
 
 
@@ -19,6 +20,36 @@ function App() {
     const settings = new Settings(repo)
     return new Controller(settings, repo);
    }, []);
+
+   const tolleSache = useMemo(() => [
+    () => controller.getSettings().setEndTime(30),
+    () => controller.addDrone(),
+    () => controller.selectDrone(0),
+    () => controller.addPositionKeyFrameNow(0, new Vector3(10, 0, 0)),
+    () => controller.addDrone(),
+    () => {controller.selectDrone(1); controller.unselectDrone(0)},
+    () => controller.addPositionKeyFrameNow(1, new Vector3(0, 0, 0)),
+    () => controller.getTimeController().setTime(10),
+    () => controller.addPositionKeyFrameNow(1, new Vector3(10, 10, 0)),
+    () => controller.addDrone(),
+   ], []);
+
+  useEffect(() => {
+    // keypress listener
+    const handleKeyPress = (event: KeyboardEvent) => {
+      if (event.key === 't') {
+        console.log('Tolle Sache drücken!');
+        const action = tolleSache.shift();
+        if (action) {
+          action();
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    }
+  }, [tolleSache]);
 
   // initSimulation liefert die Simulation-Fassade und die Scene-Komponente
   const { simulation, Scene } = useMemo(() => initSimulation(controller), []);
