5ef9bd74f7ab78b833a054f56288b14b6e795460
Pascal Kunius <uetaj@student.kit.edu>
2026-02-05 01:32:40 +0100

    Added Types to config

--- src/view/editor/config.ts
+++ src/view/editor/config.ts
@@ -1,6 +1,6 @@
 // UI Layout
-export const DRONE_EDITOR_WIDTH = "400px";
-export const DRONE_MANAGER_HEIGHT = "250px";
+export const DRONE_EDITOR_WIDTH: string = "400px";
+export const DRONE_MANAGER_HEIGHT: string = "250px";
 
 // Timeline
-export const SPEED_VALUES = [0.25, 0.5, 0.75, 1, 1.5, 2, 3];
+export const SPEED_VALUES: number[] = [0.25, 0.5, 0.75, 1, 1.5, 2, 3];
