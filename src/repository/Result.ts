d60e3c1217e9edf59294164e95b08fc1136edcf7
Luki2811 <dev@luki2811.de>
2026-02-05 12:07:36 +0100

    Docs ergänzt und CHANGES.md mit Repository-Änderungen gefüttert.

--- src/repository/Result.ts
+++ src/repository/Result.ts
@@ -1,20 +1,40 @@
+/**
+ * Wrapped ein Ergebnis mit einer möglichen Fehlermeldung
+ */
 export class Result<T> {
     private result: T
     private error: Error|undefined
 
-    constructor(data: T, error: Error|undefined = undefined) {
-        this.result = data
+    /**
+     * Konstruiert ein Ergebnis mit den übergebenen Paramtern.
+     * @param result das Ergebnis `T`
+     * @param error Eine `Error`, falls ein Fehler auftritt. Ansonsten `undefined`.
+     */
+    constructor(result: T, error: Error|undefined = undefined) {
+        this.result = result
         this.error = error
     }
 
+    /**
+     * Gibt das Ergebnis zurück. Bei Fehlermeldung wird irgendetwas übergeben.
+     * @return Das übergebene Ergebnis `T`
+     */
     getResult(): T {
         return this.result
     }
 
+    /**
+     * Untersucht, ob eine Fehlermeldung geworfen wurde.
+     * @return `true`, wenn keine Fehlermeldung geworden wurde, ansonsten `false`
+     */
     isSuccess(): boolean {
         return !this.error
     }
 
+    /**
+     * Gibt den Fehler zurück.
+     * @return `Error`, falls ein Fehler auftritt, ansonsten `undefined`.
+     */
     getError(): Error|undefined {
         return this.error
     }
