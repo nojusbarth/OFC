cbe07c5a53cba7299317d06bb7c2c8f033b19def
Nojus Barth <ugjwt@student.kit.edu>
2026-02-13 11:03:05 +0100

    Rocket Demo Show als json

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import { ProjectRepository } from './repository/ProjectRepository';
 import { Settings } from './controller/logic/Settings';
 import { Color, Vector3 } from 'three';
 import { DayTime } from './repository/entity/DayTime';
-import { PositionKeyFrame } from './repository/entity/PositionKeyFrame';
-import { ColorKeyFrame } from './repository/entity/ColorKeyFrame';
 import EditorComponent from './view/editor/EditorComponent';
 import StartpageComponent from './view/startpage/StartpageComponent';
 import { Canvas } from '@react-three/fiber';
@@ -28,67 +26,13 @@ function App() {
 
     // Configure settings
     ctrl.getSettings().setEndTime(30);
-    ctrl.getSettings().setCollisionRadius(2);
+    ctrl.getSettings().setCollisionRadius(0);
 
-    // Add drones with position keyframes
-    const drone1 = ctrl.addDrone();
-    ctrl.addPositionKeyFrame(
-      drone1,
-      new PositionKeyFrame(new Vector3(0, 0, 0), 0),
-    );
-    ctrl.getTimeController().setTime(10);
-    ctrl.addPositionKeyFrame(
-      drone1,
-      new PositionKeyFrame(new Vector3(10, 5, 0), 10),
-    );
-    ctrl.getTimeController().setTime(20);
-    ctrl.addPositionKeyFrame(
-      drone1,
-      new PositionKeyFrame(new Vector3(20, 0, 0), 20),
-    );
+    var ctr2 = new UndoableController(ctrl, new UndoRepository(), new UndoRepository());
 
-    const drone2 = ctrl.addDrone();
-    ctrl.getTimeController().setTime(0);
-    ctrl.addPositionKeyFrame(
-      drone2,
-      new PositionKeyFrame(new Vector3(0, 10, 0), 0),
-    );
-    ctrl.getTimeController().setTime(10);
-    ctrl.addPositionKeyFrame(
-      drone2,
-      new PositionKeyFrame(new Vector3(10, 15, 0), 10),
-    );
-    ctrl.getTimeController().setTime(20);
-    ctrl.addPositionKeyFrame(
-      drone2,
-      new PositionKeyFrame(new Vector3(20, 10, 0), 20),
-    );
 
-    const drone3 = ctrl.addDrone();
-    ctrl.getTimeController().setTime(0);
-    ctrl.addPositionKeyFrame(
-      drone3,
-      new PositionKeyFrame(new Vector3(5, 5, 5), 0),
-    );
-    ctrl.getTimeController().setTime(15);
-    ctrl.addPositionKeyFrame(
-      drone3,
-      new PositionKeyFrame(new Vector3(15, 10, 5), 15),
-    );
 
-    // Add color keyframes
-    ctrl.getTimeController().setTime(0);
-    ctrl.addColorKeyFrame(drone1, new ColorKeyFrame(new Color(1, 0, 0), 0)); // Red
-    ctrl.addColorKeyFrame(drone2, new ColorKeyFrame(new Color(0, 1, 0), 0)); // Green
-    ctrl.addColorKeyFrame(drone3, new ColorKeyFrame(new Color(0, 0, 1), 0)); // Blue
-
-    ctrl.getTimeController().setTime(10);
-    ctrl.addColorKeyFrame(drone1, new ColorKeyFrame(new Color(1, 1, 0), 10)); // Yellow
-
-    // Reset time to 0
-    ctrl.getTimeController().setTime(0);
-
-    return new UndoableController(ctrl, new UndoRepository(), new UndoRepository());
+    return ctr2;
   }, []);
 
   const tolleSache = useMemo(() => [
