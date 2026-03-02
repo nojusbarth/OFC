b1ace1db83adf73a983773f49d3b04f632634520
Pascal Kunius <uetaj@student.kit.edu>
2026-03-02 15:12:00 +0100

    Fixes

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,6 +72,12 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   url: 'http://localhost:3000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
   webServer: {
     command: process.env.CI
       ? 'npm run build && npx serve -s build -l 3000'
