383f9747b26e99676d91b71268597c454248d9b8
Moritz Nickel <ushnn@student.kit.edu>
2026-03-05 14:31:48 +0100

    fix worker

--- src/controller/logic/CollisionHandlerInterface.ts
+++ src/controller/logic/CollisionHandlerInterface.ts
@@ -68,7 +68,7 @@ async function getCollisionWorker(): Promise<Worker | null> {
     }
 
     try {
-        collisionWorker = bindWorker(new Worker(new URL("./CollisionHandler.worker.ts", window.location.href)));
+        collisionWorker = bindWorker(new Worker(new URL("./CollisionHandler.worker.ts", import.meta.url), { type: "module" }));
     } catch {
         collisionWorker = null;
     }
